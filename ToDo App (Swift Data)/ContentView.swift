4dd28392af49325ef29a36e944a83fe7828a4bff
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2024-03-22 13:28:28 +0100

    Initial Commit

---
+++ ToDo App (Swift Data)/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ToDo App (Swift Data)
+//
+//  Created by Richard Uzor on 22/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDo App (Swift Data)/ToDo_App__Swift_Data_App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_App__Swift_Data_App.swift
+//  ToDo App (Swift Data)
+//
+//  Created by Richard Uzor on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_App__Swift_Data_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
